49376c5fee16aed14bff6540c74b484815a589e5
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-10-19 17:51:18 +0500

    feat: update Task.stories.tsx imports

--- src/features/TodolistList/Todolist/Task/Task.stories.tsx
+++ src/features/TodolistList/Todolist/Task/Task.stories.tsx
@@ -1,12 +1,9 @@
 import { Meta, StoryObj } from '@storybook/react'
 import { Task } from './Task'
-import {
-  ReduxStoreProviderDecorator,
-  todolistID1,
-} from '../../../../stories/ReduxStoreProviderDecorator'
+import { ReduxStoreProviderDecorator, todolistID1 } from 'stories/ReduxStoreProviderDecorator'
 import { useSelector } from 'react-redux'
-import { AppRootStateT } from '../../../../app/store'
-import { TaskT } from '../../../../api/tasks-api'
+import { AppRootStateT } from 'app/store'
+import { TaskT } from 'api/tasks-api'
 
 const meta: Meta<typeof Task> = {
   title: 'Todolists/Task',
