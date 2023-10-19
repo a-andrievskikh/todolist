e3ebe0ad38d5a3a3f267d2e623019413ecd8954a
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-10-19 17:50:31 +0500

    feat: update ErrorSnackbar.tsx imports

--- src/components/ErrorSnackbar/ErrorSnackbar.tsx
+++ src/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -1,8 +1,8 @@
 import { forwardRef, SyntheticEvent } from 'react'
 import Snackbar from '@mui/material/Snackbar'
 import MuiAlert, { AlertProps } from '@mui/material/Alert'
-import { useAppDispatch, useAppSelector } from '../../app/hooks'
-import { setAppErrorAC } from '../../app/app-reducer'
+import { useAppDispatch, useAppSelector } from 'app/hooks'
+import { setAppErrorAC } from 'app/app-reducer'
 
 const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant='filled' {...props} />
