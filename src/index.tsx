b1d897d77d4e0524ef709fd9543801709d09020e
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-10-20 16:23:40 +0500

    ref: index ReactDOM.render

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import './index.css'
 import { App } from 'app/App'
 import { Provider } from 'react-redux'
 import { store } from 'app/store'
 import { BrowserRouter } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
+ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
+  </Provider>,
+  document.getElementById('root') as HTMLElement
 )
