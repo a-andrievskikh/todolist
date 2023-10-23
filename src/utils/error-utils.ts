5b1a58740b6834464ee4f02153b73a2fa770bf22
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-10-23 14:19:05 +0500

    ref: type of args

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -4,24 +4,18 @@ import { ResponseT } from 'api/common-types'
 
 const errorText = 'Some error occurred'
 
-export const handleServerAppError = <D>(
-  dispatch: Dispatch<ErrorUtilsDispatchT>,
-  data: ResponseT<D>
-) => {
+export const handleServerAppError = <D>(dispatch: Dispatch<ErrorUtilsDispatchT>, data: ResponseT<D>) => {
   if (data.messages.length) {
-    dispatch(setAppErrorAC(data.messages[0]))
+    dispatch(setAppErrorAC({ error: data.messages[0] }))
   } else {
-    dispatch(setAppErrorAC(errorText))
+    dispatch(setAppErrorAC({ error: errorText }))
   }
-  dispatch(setAppStatusAC('failed'))
+  dispatch(setAppStatusAC({ status: 'failed' }))
 }
 
-export const handleServerNetworkError = (
-  dispatch: Dispatch<ErrorUtilsDispatchT>,
-  error: string
-) => {
-  dispatch(setAppErrorAC(error ? error : errorText))
-  dispatch(setAppStatusAC('failed'))
+export const handleServerNetworkError = (dispatch: Dispatch<ErrorUtilsDispatchT>, error: string) => {
+  dispatch(setAppErrorAC({ error } ? { error } : { error: errorText }))
+  dispatch(setAppStatusAC({ status: 'failed' }))
 }
 
 // Types
