052562628ff53a0f87c4a597a7954ba7fd1f4bad
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-10-25 17:18:35 +0500

    feat: add slice and rn app-reducer

---
+++ src/app/app-slice.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+const slice = createSlice({
+  name: 'app',
+  initialState: {
+    status: 'loading' as RequestStatusT,
+    error: null as ResponseErrorT,
+    isInitialized: false,
+    //'Error message 😠'
+  },
+  reducers: {
+    setAppStatus: (state, action: PayloadAction<{ status: RequestStatusT }>) => {
+      state.status = action.payload.status
+    },
+    setAppError: (state, action: PayloadAction<{ error: ResponseErrorT }>) => {
+      state.error = action.payload.error
+    },
+    setInitialized: (state, action: PayloadAction<{ isInitialized: boolean }>) => {
+      state.isInitialized = action.payload.isInitialized
+    },
+  },
+})
+
+export const appSlice = slice.reducer
+export const appActions = slice.actions
+
+// Types
+export type AppStateT = ReturnType<typeof slice.getInitialState>
+export type RequestStatusT = 'idle' | 'loading' | 'succeeded' | 'failed'
+export type ResponseErrorT = string | null
