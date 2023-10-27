8c4d14af6c3eea856963cce4ce160733037bdd15
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-10-27 15:42:32 +0500

    feat: add useTask

--- src/features/TodolistList/Todolist/Task/Task.tsx
+++ src/features/TodolistList/Todolist/Task/Task.tsx
@@ -1,38 +1,13 @@
 import Delete from '@mui/icons-material/Delete'
 import Checkbox from '@mui/material/Checkbox'
 import IconButton from '@mui/material/IconButton'
-import { deleteTaskTC, updateTaskTC } from 'features/TodolistList/tasks-slice'
 import { EditableSpan } from 'components/EditableSpan/EditableSpan'
-import { ChangeEvent, memo, useCallback } from 'react'
+import { memo } from 'react'
 import { TaskStatuses } from 'api/tasks-api'
-import { useAppDispatch } from 'app/hooks/useAppDispatch'
+import { useTask } from 'features/TodolistList/Todolist/Task/hooks/useTask'
 
-export const Task = memo(({ todolistID, taskID, title, status }: TaskPropsT) => {
-  const dispatch = useAppDispatch()
-
-  const deleteTask = useCallback(() => {
-    dispatch(deleteTaskTC(todolistID, taskID))
-  }, [dispatch, todolistID, taskID])
-
-  const updateTaskStatus = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      dispatch(
-        updateTaskTC(todolistID, taskID, {
-          status: e.currentTarget.checked ? TaskStatuses.Completed : TaskStatuses.New,
-        })
-      )
-    },
-    [dispatch, todolistID, taskID]
-  )
-
-  const updateTaskTitle = useCallback(
-    (taskTitle: string) => {
-      dispatch(updateTaskTC(todolistID, taskID, { title: taskTitle }))
-    },
-    [dispatch, todolistID, taskID]
-  )
-
-  const listItemClassName = status === TaskStatuses.Completed ? 'isDone' : ''
+export const Task = memo(({ todolistID, taskID, title, status }: TaskPT) => {
+  const { deleteTask, updateTaskStatus, updateTaskTitle, listItemClassName } = useTask(todolistID, taskID, status)
 
   return (
     <li className={listItemClassName} key={taskID}>
@@ -46,7 +21,7 @@ export const Task = memo(({ todolistID, taskID, title, status }: TaskPropsT) => 
 })
 
 // Types
-export type TaskPropsT = {
+export type TaskPT = {
   todolistID: string
   taskID: string
   title: string
---
+++ src/features/TodolistList/Todolist/Task/hooks/useTask.ts
@@ -0,0 +1,34 @@
+import { useAppDispatch } from 'app/hooks/useAppDispatch'
+import { ChangeEvent, useCallback } from 'react'
+import { deleteTaskTC, updateTaskTC } from 'features/TodolistList/tasks-slice'
+import { TaskStatuses } from 'api/tasks-api'
+
+export const useTask = (todolistID: string, taskID: string, status: TaskStatuses) => {
+  const dispatch = useAppDispatch()
+
+  const deleteTask = useCallback(() => {
+    dispatch(deleteTaskTC(todolistID, taskID))
+  }, [dispatch, todolistID, taskID])
+
+  const updateTaskStatus = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      dispatch(
+        updateTaskTC(todolistID, taskID, {
+          status: e.currentTarget.checked ? TaskStatuses.Completed : TaskStatuses.New,
+        })
+      )
+    },
+    [dispatch, todolistID, taskID]
+  )
+
+  const updateTaskTitle = useCallback(
+    (taskTitle: string) => {
+      dispatch(updateTaskTC(todolistID, taskID, { title: taskTitle }))
+    },
+    [dispatch, todolistID, taskID]
+  )
+
+  const listItemClassName = status === TaskStatuses.Completed ? 'isDone' : ''
+
+  return { deleteTask, updateTaskStatus, updateTaskTitle, listItemClassName }
+}
