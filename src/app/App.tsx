46dadd54da043b3d71899aaf78632a4a4c30b4bf
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-10-27 11:39:25 +0500

    ref: set progress to a separate variable

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,16 +9,16 @@ import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import CircularProgress from '@mui/material/CircularProgress'
 import LinearProgress from '@mui/material/LinearProgress'
-import { TodolistList } from 'features/TodolistList/TodolistList'
-import { Auth } from 'features/Auth/Auth'
 import { ErrorSnackbar } from 'components/ErrorSnackbar/ErrorSnackbar'
-import { Navigate, Route, Routes } from 'react-router-dom'
 import { useApp } from 'app/useApp'
+import { AppRoutes } from 'app/AppRoutes'
 
 export const App = memo(({ demo = false }: AppPT) => {
   const { status, isInitialized, isLoggedIn, logOut } = useApp()
 
   const progressStyles: React.CSSProperties = { position: 'fixed', top: '30%', textAlign: 'center', width: '100%' }
+
+  const progressView = status === 'loading' && <LinearProgress color={'secondary'} />
 
   if (!isInitialized) {
     return (
@@ -45,15 +45,10 @@ export const App = memo(({ demo = false }: AppPT) => {
             </Button>
           )}
         </Toolbar>
-        {status === 'loading' && <LinearProgress color={'secondary'} />}
+        {progressView}
       </AppBar>
       <Container fixed>
-        <Routes>
-          <Route path={'/'} element={<TodolistList demo={demo} />} />
-          <Route path={'/login'} element={<Auth />} />
-          <Route path={'404'} element={<h1>404: PAGE NOT FOUND</h1>} />
-          <Route path={'*'} element={<Navigate to={'404'} />} />
-        </Routes>
+        <AppRoutes />
       </Container>
     </div>
   )
