62d3c9041a2060ec80f6f10b27a6b4c93e8f6933
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-10-27 11:37:43 +0500

    feat: add AppRoutes

---
+++ src/app/AppRoutes.tsx
@@ -0,0 +1,14 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { TodolistList } from 'features/TodolistList/TodolistList'
+import { Auth } from 'features/Auth/Auth'
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path={'/'} element={<TodolistList demo={demo} />} />
+      <Route path={'/login'} element={<Auth />} />
+      <Route path={'404'} element={<h1>404: PAGE NOT FOUND</h1>} />
+      <Route path={'*'} element={<Navigate to={'404'} />} />
+    </Routes>
+  )
+}
