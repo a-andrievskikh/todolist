597ef9ca7190c8d3a6c616001326f76cb7fde2bd
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-11-03 18:36:30 +0500

    feat: add index for components

---
+++ src/common/components/index.ts
@@ -0,0 +1,4 @@
+export { EditableSpan } from 'common/components/EditableSpan/EditableSpan'
+export { ErrorSnackbar } from 'common/components/ErrorSnackbar/ErrorSnackbar'
+export { Header } from 'common/components/Header/Header'
+export { ItemForm } from 'common/components/ItemForm/ItemForm'
