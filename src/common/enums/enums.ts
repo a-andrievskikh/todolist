2e92624f9c828ca31c6cf8f7de14cc1b7a9a6f2d
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-11-04 16:01:15 +0500

    feat: add enums.ts

---
+++ src/common/enums/enums.ts
@@ -0,0 +1,20 @@
+export enum TaskStatuses {
+  New = 0,
+  InProgress = 1,
+  Completed = 2,
+  Dragt = 3,
+}
+
+export enum TaskPriorities {
+  Low = 0,
+  Middle = 1,
+  High = 2,
+  Urgently = 3,
+  Later = 4,
+}
+
+export enum ResultCodes {
+  Success = 0,
+  Failed = 1,
+  Captcha = 10,
+}
