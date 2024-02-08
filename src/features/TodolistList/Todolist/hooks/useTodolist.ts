5042090794917cfa4df20d4bef337017da5da59e
a-andrievskikh <sn0abg0@gmail.com>
2024-02-08 18:28:01 +0500

    ref: format code

--- src/features/TodolistList/Todolist/hooks/useTodolist.ts
+++ src/features/TodolistList/Todolist/hooks/useTodolist.ts
@@ -1,41 +1,38 @@
-import { deleteTodolistTC, FilterT, todolistsActions, updateTodolistTC } from 'features/TodolistList/todolists-reducer'
-import { useAppDispatch } from 'app/hooks/useAppDispatch'
-import { useCallback, useEffect } from 'react'
-import { createTaskTC, tasksThunks } from 'features/TodolistList/tasks-reducer'
+import { FilterT, todolistsActions, todolistsThunks } from 'features/TodolistList/todolists-reducer'
+import { useAppDispatch } from 'common/hooks/useAppDispatch'
+import { useCallback } from 'react'
+import { tasksThunks } from 'features/TodolistList/tasks-reducer'
 import { TodolistPT } from 'features/TodolistList/Todolist/Todolist'
 
 export const useTodolist = ({ todolist, demo }: TodolistPT) => {
   const dispatch = useAppDispatch()
 
-  useEffect(() => {
-    if (demo) return
-    dispatch(tasksThunks.getTasks(todolist.id))
-  }, [dispatch, demo, todolist.id])
-
   const addItem = useCallback(
     (itemTitle: string) => {
-      dispatch(createTaskTC(todolist.id, itemTitle))
+      dispatch(tasksThunks.createTask({ todolistID: todolist.id, title: itemTitle }))
     },
     [dispatch, todolist.id]
   )
 
   const updateTodolistTitle = useCallback(
     (newTitle: string) => {
-      dispatch(updateTodolistTC(todolist.id, newTitle))
+      dispatch(todolistsThunks.updateTodolistTitle({ todolistID: todolist.id, title: newTitle }))
     },
     [dispatch, todolist.id]
   )
 
   const updateTodolistFilter = useCallback(
     (filterValue: FilterT) => {
-      dispatch(todolistsActions.updateTodolistFilter({ todolistID: todolist.id, filter: filterValue }))
+      dispatch(
+        todolistsActions.updateTodolistFilter({ todolistID: todolist.id, filter: filterValue })
+      )
     },
     [dispatch, todolist.id]
   )
 
   const deleteTodolist = useCallback(
     (todolistID: string) => {
-      dispatch(deleteTodolistTC(todolistID))
+      dispatch(todolistsThunks.deleteTodolist({ todolistID }))
     },
     [dispatch]
   )
