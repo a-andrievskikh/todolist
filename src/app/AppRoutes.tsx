026052079c51aabd2635c31335748b94afbf243c
a-andrievskikh <sn0abg0@gmail.com>
2024-02-26 17:28:13 +0500

    chore: remove to ui path

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -1,12 +1,12 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { TodolistList } from 'features/TodolistList/TodolistList'
-import { Auth } from 'features/Auth/Auth'
+import { Auth } from 'features/Auth/ui/Auth'
 
 export const AppRoutes = ({ demo = false }: AppRoutesPT) => {
   return (
     <Routes>
       <Route path={'/'} element={<TodolistList demo={demo} />} />
-      <Route path={'/login'} element={<Auth />} />
+      <Route path={'login'} element={<Auth />} />
       <Route path={'404'} element={<h1>404: PAGE NOT FOUND</h1>} />
       <Route path={'*'} element={<Navigate to={'404'} />} />
     </Routes>
