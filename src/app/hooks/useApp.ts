da74191c9615cabdf2cc56e347581433fbcbb756
a-andrievskikh <sn0abg0@gmail.com>
2024-04-21 17:37:11 +0500

    chore: rename logout

--- src/app/hooks/useApp.ts
+++ src/app/hooks/useApp.ts
@@ -1,28 +1,28 @@
-import { useAppDispatch } from 'common/hooks/useAppDispatch'
 import { useAppSelector } from 'common/hooks/useAppSelector'
 import { isInitializedSelector, statusSelector } from 'app/model/app-selectors'
 import { authThunks } from 'features/Auth/model/auth-reducer'
 import { useEffect } from 'react'
 import { isLoggedInSelector } from 'features/Auth/model/auth-selectors'
 import { RequestStatusT } from 'app/types/app-types'
+import { useActions } from 'common/hooks/useActions'
 
 export const useApp = () => {
   const status = useAppSelector<RequestStatusT>(statusSelector)
   const isInitialized = useAppSelector<boolean>(isInitializedSelector)
   const isLoggedIn = useAppSelector<boolean>(isLoggedInSelector)
 
-  const dispatch = useAppDispatch()
+  const { logout, initializeApp } = useActions(authThunks)
 
-  const logOutHandler = () => dispatch(authThunks.logout())
+  const logoutHandler = () => logout()
 
   useEffect(() => {
-    dispatch(authThunks.initializeApp())
-  }, [dispatch])
+    initializeApp()
+  }, [])
 
   return {
     status,
     isInitialized,
     isLoggedIn,
-    logOut: logOutHandler,
+    logout: logoutHandler,
   }
 }
