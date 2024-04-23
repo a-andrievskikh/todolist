41ad2664f5f9a24e749c10b1c93becc26b858c32
a-andrievskikh <sn0abg0@gmail.com>
2024-04-23 19:21:38 +0500

    chore: change useDispatch to useActions

--- src/features/TodolistList/Todolist/hooks/useTodolist.ts
+++ src/features/TodolistList/Todolist/hooks/useTodolist.ts
@@ -2,43 +2,45 @@ import {
   todolistsActions,
   todolistsThunks,
 } from 'features/TodolistList/Todolist/model/todolist-reducer'
-import { useAppDispatch } from 'common/hooks/useAppDispatch'
 import { useCallback } from 'react'
 import { tasksThunks } from 'features/TodolistList/Todolist/Task/model/tasks-reducer'
 import { FilterT, TodolistPT } from 'features/TodolistList/Todolist/types/todolist-types'
+import { useActions } from 'common/hooks/useActions'
 
 export const useTodolist = ({ todolist, demo }: TodolistPT) => {
-  const dispatch = useAppDispatch()
+  const { createTask, updateTodolistTitle, updateTodolistFilter, deleteTodolist } = useActions({
+    ...tasksThunks,
+    ...todolistsThunks,
+    ...todolistsActions,
+  })
 
   const addItem = useCallback(
-    (itemTitle: string) => {
-      dispatch(tasksThunks.createTask({ todolistID: todolist.id, title: itemTitle }))
-    },
-    [dispatch, todolist.id]
+    (itemTitle: string) => createTask({ todolistID: todolist.id, title: itemTitle }),
+    [createTask, todolist.id]
   )
 
-  const updateTodolistTitle = useCallback(
+  const updateTodolistTitleCallBack = useCallback(
     (newTitle: string) => {
-      dispatch(todolistsThunks.updateTodolistTitle({ todolistID: todolist.id, title: newTitle }))
+      updateTodolistTitle({ todolistID: todolist.id, title: newTitle })
     },
-    [dispatch, todolist.id]
+    [updateTodolistTitle, todolist.id]
   )
 
-  const updateTodolistFilter = useCallback(
-    (filterValue: FilterT) => {
-      dispatch(
-        todolistsActions.updateTodolistFilter({ todolistID: todolist.id, filter: filterValue })
-      )
-    },
-    [dispatch, todolist.id]
+  const updateTodolistFilterCallBack = useCallback(
+    (filterValue: FilterT) =>
+      updateTodolistFilter({ todolistID: todolist.id, filter: filterValue }),
+    [updateTodolistFilter, todolist.id]
   )
 
-  const deleteTodolist = useCallback(
-    (todolistID: string) => {
-      dispatch(todolistsThunks.deleteTodolist({ todolistID }))
-    },
-    [dispatch]
+  const deleteTodolistCallBack = useCallback(
+    (todolistID: string) => deleteTodolist({ todolistID }),
+    [deleteTodolist]
   )
 
-  return { addItem, updateTodolistTitle, updateTodolistFilter, deleteTodolist }
+  return {
+    addItem,
+    updateTodolistTitleCallBack,
+    updateTodolistFilterCallBack,
+    deleteTodolistCallBack,
+  }
 }
