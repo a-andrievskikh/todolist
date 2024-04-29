039699b5b1e8003943566ffc18b016e1f9c4e618
a-andrievskikh <sn0abg0@gmail.com>
2024-04-29 18:16:39 +0500

    chore: change combineReducers to configureStore

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,23 +1,20 @@
-import { AnyAction, combineReducers } from 'redux'
-import { tasksReducer } from 'features/TodolistList/tasks-reducer'
-import { todolistsReducer } from 'features/TodolistList/todolists-reducer'
-import { ThunkAction, ThunkDispatch } from 'redux-thunk'
-import { appReducer } from 'app/app-reducer'
+import { tasksReducer } from 'features/TodolistList/Todolist/Task/model/tasks-reducer'
+import { todolistReducer } from 'features/TodolistList/Todolist/model/todolist-reducer'
+import { appReducer } from 'app/model/app-reducer'
 import { authReducer } from 'features/Auth/model/auth-reducer'
 import { configureStore } from '@reduxjs/toolkit'
 
-export const rootReducer = combineReducers({
-  tasks: tasksReducer,
-  todolists: todolistsReducer,
-  app: appReducer,
-  auth: authReducer,
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+    todolists: todolistReducer,
+    app: appReducer,
+    auth: authReducer,
+  },
 })
 
-export const store = configureStore({ reducer: rootReducer })
-
 export type AppRootStateT = ReturnType<typeof store.getState>
-export type AppDispatch = ThunkDispatch<AppRootStateT, unknown, AnyAction>
-export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateT, unknown, AnyAction>
+export type AppDispatch = typeof store.dispatch
 
 // @ts-ignore
 window.store = store
