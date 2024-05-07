85a844f53315fbb9d005d4aa2cdcbdb88537333b
a-andrievskikh <sn0abg0@gmail.com>
2024-05-07 14:11:20 +0500

    chore: delete unused a hook useAuth.ts

--- src/features/Auth/hooks/useAuth.ts
+++
@@ -1,7 +0,0 @@
-import { isLoggedInSelector } from 'features/Auth/model/auth-selectors'
-import { useAppSelector } from 'common/hooks/useAppSelector'
-
-export const useAuth = () => {
-  const isLoggedIn = useAppSelector<boolean>(isLoggedInSelector)
-  return { isLoggedIn }
-}
