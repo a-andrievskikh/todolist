8b7537a17576aa020df89d5212ba851eedd49f2f
a-andrievskikh <sn0abg0@gmail.com>
2024-05-08 18:22:45 +0500

    chore: move to /lib/hooks

--- src/features/TodolistList/lib/hooks/useTodolistList.ts
+++ src/features/TodolistList/lib/hooks/useTodolistList.ts
@@ -1,23 +1,26 @@
-import { useAppSelector } from 'common/hooks/useAppSelector'
-import { todolistsThunks } from 'features/TodolistList/Todolist/model/todolist-reducer'
+import { useAppSelector } from 'shared/lib'
+import { useActions } from 'shared/lib'
+import { todolistsThunks } from 'features/TodolistList/ui/Todolist/model/todolist-reducer'
 import { todolistsSelector } from 'features/TodolistList/model/todolistList-selector'
 import { isLoggedInSelector } from 'features/Auth/model/auth-selectors'
 import { useCallback, useEffect } from 'react'
-import { TodolistDomainT } from 'features/TodolistList/Todolist/types/todolist-types'
+import { TodolistDomainT } from 'features/TodolistList/ui/Todolist/types/todolist-types'
 import { TodolistListTypes } from 'features/TodolistList/types/todolistList-types'
-import { useActions } from 'common/hooks/useActions'
 
 export const useTodolistList = ({ demo }: TodolistListTypes) => {
   const { createTodolist, getTodolists } = useActions(todolistsThunks)
   const todolists = useAppSelector<TodolistDomainT[]>(todolistsSelector)
   const isLoggedIn = useAppSelector<boolean>(isLoggedInSelector)
 
-  const addTodolist = useCallback((todolistTitle: string) => createTodolist(todolistTitle), [])
+  const addTodolist = useCallback(
+    (todolistTitle: string) => createTodolist(todolistTitle),
+    [createTodolist]
+  )
 
   useEffect(() => {
     if (demo || !isLoggedIn) return
     getTodolists()
-  }, [demo, isLoggedIn])
+  }, [demo, isLoggedIn, getTodolists])
 
   return { todolists, isLoggedIn, addTodolist }
 }
