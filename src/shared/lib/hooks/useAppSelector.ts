ae361f76b4b71751b71f493e8ba9e4b527624797
a-andrievskikh <sn0abg0@gmail.com>
2024-05-29 20:56:47 +0500

    chore: move to /store/reduxHooks

--- src/shared/lib/hooks/useAppSelector.ts
+++
@@ -1,4 +0,0 @@
-import { TypedUseSelectorHook, useSelector } from 'react-redux'
-import type { AppRootStateT } from 'app/store'
-
-export const useAppSelector: TypedUseSelectorHook<AppRootStateT> = useSelector
