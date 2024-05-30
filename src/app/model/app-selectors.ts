30a94eca02ebfe4bbc4541d60f232cc5e53d6226
a-andrievskikh <sn0abg0@gmail.com>
2024-05-30 19:26:53 +0500

    chore: delete

--- src/app/model/app-selectors.ts
+++
@@ -1,4 +0,0 @@
-import { AppRootStateT } from 'app/store'
-
-export const statusSelector = (s: AppRootStateT) => s.app.status
-export const isInitializedSelector = (s: AppRootStateT) => s.app.isInitialized
