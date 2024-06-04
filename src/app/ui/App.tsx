1fb370a321757f71d04bc3897310bc17045c94a7
a-andrievskikh <sn0abg0@gmail.com>
2024-06-04 19:05:50 +0500

    chore: move to /app/ui

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -1,5 +1,3 @@
-import 'app/ui/App.module.css'
-import { CSSProperties, memo } from 'react'
 import Container from '@mui/material/Container'
 import AppBar from '@mui/material/AppBar'
 import Menu from '@mui/icons-material/Menu'
@@ -9,32 +7,35 @@ import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import CircularProgress from '@mui/material/CircularProgress'
 import LinearProgress from '@mui/material/LinearProgress'
-import { useApp } from 'app/lib/hooks/useApp'
 import { AppRoutes } from 'app/ui/AppRoutes'
 import { ErrorSnackbar } from 'shared/ui'
-import { AppProps } from 'app/model/types/app.types'
+import s from './App.module.css'
+import { appSelectors } from 'app/model'
+import { authSelectors, authThunks } from 'features/auth/model'
+import { useActions, useAppSelector } from 'app/store'
+import type { RequestStatus } from 'shared/types'
+import { useEffect } from 'react'
 
-export const App = memo(({ demo = false }: AppProps) => {
-  const { status, isInitialized, isLoggedIn, logout } = useApp()
+type Props = { demo?: boolean }
 
-  const progressStyles: CSSProperties = {
-    position: 'fixed',
-    top: '30%',
-    textAlign: 'center',
-    width: '100%',
-  }
+export const App = ({ demo = false }: Props) => {
+  const { logout, initializeApp } = useActions(authThunks)
+  const { statusSelector, isInitializedSelector } = appSelectors
+  const { isLoggedInSelector } = authSelectors
+  const status = useAppSelector<RequestStatus>(statusSelector)
+  const isInitialized = useAppSelector<boolean>(isInitializedSelector)
+  const isLoggedIn = useAppSelector<boolean>(isLoggedInSelector)
+  const logoutHandler = () => logout()
 
-  const toolbarStyles: CSSProperties = {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  }
+  useEffect(() => {
+    initializeApp()
+  }, [initializeApp])
 
   const progressView = status === 'loading' && <LinearProgress color={'secondary'} />
 
   if (!isInitialized) {
     return (
-      <div style={progressStyles}>
+      <div className={s.progress}>
         <CircularProgress />
       </div>
     )
@@ -44,13 +45,13 @@ export const App = memo(({ demo = false }: AppProps) => {
     <div className='App'>
       <ErrorSnackbar />
       <AppBar position={'static'}>
-        <Toolbar style={toolbarStyles}>
+        <Toolbar className={s.toolbar}>
           <IconButton>
             <Menu />
           </IconButton>
           <Typography variant={'h6'}>TODOLIST</Typography>
           {isLoggedIn && (
-            <Button color={'warning'} onClick={logout}>
+            <Button color={'warning'} onClick={logoutHandler}>
               Logout
             </Button>
           )}
@@ -62,4 +63,4 @@ export const App = memo(({ demo = false }: AppProps) => {
       </Container>
     </div>
   )
-})
+}
