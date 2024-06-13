55703da3ff89e860861c1f5b614bf325110e4ad1
a-andrievskikh <sn0abg0@gmail.com>
2024-06-13 18:45:22 +0500

    chore: add index for store

---
+++ src/app/store/index.ts
@@ -0,0 +1,8 @@
+export type { AppRootState, AppDispatch } from './reduxHooks'
+export { useAppDispatch, useActions, useAppSelector } from './reduxHooks'
+export { store } from './store'
+export {
+  ReduxStoreProviderDecorator,
+  todolistID1,
+  todolistID2,
+} from './ReduxStoreProviderDecorator'
