d5f7c8c770c4c5c5ff3b72120c19ae330355472c
a-andrievskikh <sn0abg0@gmail.com>
2024-06-14 19:07:40 +0500

    chore: delete

--- src/common/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { EditableSpan } from 'common/components/EditableSpan/EditableSpan'
-export { ErrorSnackbar } from 'common/components/ErrorSnackbar/ErrorSnackbar'
-export { Header } from 'common/components/Header/Header'
-export { ItemForm } from 'common/components/ItemForm/ItemForm'
