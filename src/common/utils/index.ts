b82a9f64a9e49136df7ac0a02d05e97777c15b64
a-andrievskikh <sn0abg0@gmail.com>
2024-06-15 14:20:46 +0500

    chore: delete

--- src/common/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export { createAppAsyncThunk } from 'common/utils/create-app-async-thunk'
-export { handleServerAppError } from 'common/utils/handle-server-app-error'
-export { handleServerNetworkError } from 'common/utils/handle-server-network-error'
