8318eb6413adc37d7ad64f8f76c5fcfa33b4955e
a-andrievskikh <sn0abg0@gmail.com>
2024-06-18 21:17:15 +0500

    chore: remove props for AppRoutes

--- src/app/ui/AppRoutes.tsx
+++ src/app/ui/AppRoutes.tsx
@@ -2,12 +2,10 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { TodolistsList } from 'features/todolists-list/ui/TodolistsList'
 import { Auth } from 'features/auth/ui'
 
-type Props = { demo?: boolean }
-
-export const AppRoutes = ({ demo = false }: Props) => {
+export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path={'/'} element={<TodolistsList demo={demo} />} />
+      <Route path={'/'} element={<TodolistsList demo={false} />} />
       <Route path={'login'} element={<Auth />} />
       <Route path={'404'} element={<h1>404: PAGE NOT FOUND</h1>} />
       <Route path={'*'} element={<Navigate to={'404'} />} />
