14061d016142d37ef822037b1fd4134988e15005
a-andrievskikh <sn0abg0@gmail.com>
2024-06-27 19:34:57 +0500

    chore: add headers properties

--- src/shared/api/instance.ts
+++ src/shared/api/instance.ts
@@ -1,6 +1,11 @@
 import axios from 'axios'
+import { authApiKey, authData } from 'authData'
 
 export const instance = axios.create({
   baseURL: 'https://social-network.samuraijs.com/api/1.1/',
   withCredentials: true,
+  headers: {
+    'API-KEY': authApiKey,
+    Authorization: authData,
+  },
 })
