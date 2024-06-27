f62c5423f2e272c64c70fab01c36295a55a29793
a-andrievskikh <sn0abg0@gmail.com>
2024-06-27 19:33:24 +0500

    chore: change imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import { App } from 'app/ui/App'
+import { App } from 'app/ui'
 import { Provider } from 'react-redux'
-import { store } from 'app/store'
 import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import { store } from 'app/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
+// const rerenderEntireTree = () => {
 root.render(
   <Provider store={store}>
     <BrowserRouter>
@@ -14,3 +15,12 @@ root.render(
     </BrowserRouter>
   </Provider>
 )
+// }
+
+// rerenderEntireTree()
+//
+// if (process.env.NODE_ENV === 'development' && (module as any).hot) {
+//   ;(module as any).hot.accept('./app/ui', () => {
+//     rerenderEntireTree()
+//   })
+// }
