63dab66ea81b186566b34e12413a716be9283a87
a-andrievskikh <sn0abg0@gmail.com>
2024-07-13 13:35:13 +0500

    chore: change imports, reformat TodolistWithRedux func

--- src/features/todolists-list/ui/todolist/Todolist.stories.tsx
+++ src/features/todolists-list/ui/todolist/Todolist.stories.tsx
@@ -1,9 +1,8 @@
 import { Meta, StoryObj } from '@storybook/react'
-import { Todolist } from './Todolist'
-import { ReduxStoreProviderDecorator } from 'stories/ReduxStoreProviderDecorator'
+import { Todolist } from 'features/todolists-list/ui/todolist'
+import { type AppRootState, ReduxStoreProviderDecorator } from 'app/store'
 import { useSelector } from 'react-redux'
-import { AppRootStateT } from 'app/store'
-import { TodolistDomainT } from 'features/TodolistList/todolists-reducer'
+import { TodolistDomain } from 'shared/types'
 
 const meta: Meta<typeof Todolist> = {
   title: 'Todolists/Todolist',
@@ -16,10 +15,14 @@ export default meta
 type Story = StoryObj<typeof Todolist>
 
 const TodolistWithRedux = () => {
-  const todolist = useSelector<AppRootStateT, TodolistDomainT>(state => state.todolists[0])
+  const todolist = useSelector<AppRootState, TodolistDomain>(state => state.todolists[0])
   const demo = true
 
-  return todolist ? <Todolist todolist={todolist} demo={demo} /> : <>Todolist have expired. Restart Storybook</>
+  return todolist ? (
+    <Todolist todolist={todolist} demo={demo} />
+  ) : (
+    <>Todolist have expired. Restart Storybook</>
+  )
 }
 
 export const TodolistStory: Story = {
